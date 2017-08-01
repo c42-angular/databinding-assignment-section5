faebc7773cebc4dbb480fe3e9ae94d7f0a529efc
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2017-08-01 20:41:06 +0100

    Alternate evens & odds!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,14 @@ import { GameCounter } from 'app/shared/game-counter.model';
 export class AppComponent {
   gameNumbersOdd: GameCounter[] = [];
   gameNumbersEven: GameCounter[] = [];
+  gameNumbers: GameCounter[] = [];
 
-  onGameTimerElapsed(eventArg: GameCounter){
+  onGameTimerElapsed(eventArg: GameCounter) {
     console.log('Event subscribed in app component: ');
     console.log(eventArg);
+
+    this.gameNumbers.push(eventArg);
+
     if (eventArg.numeral % 2 === 0) {
       this.gameNumbersEven.push(eventArg);
     } else {
